2e9926851b9c77270c021b56cc4305980acaa027
Sergey Lazchenko <qirsam@gmail.com>
2022-12-26 21:29:42 +0300

    add values

--- src/main/resources/db.changelog/db.changelog-1.0.sql
+++ src/main/resources/db.changelog/db.changelog-1.0.sql
@@ -27,5 +27,21 @@ CREATE TABLE IF NOT EXISTS film
     name         VARCHAR(128)                                    NOT NULL,
     date_release DATE,
     studio_id    BIGINT REFERENCES studio (id) ON DELETE CASCADE NOT NULL
+);
 
-)
+--changeset qirsam:3
+CREATE TABLE IF NOT EXISTS actor_film
+(
+    id SERIAL NOT NULL PRIMARY KEY ,
+    actor_id BIGINT NOT NULL REFERENCES actor (id) ON DELETE CASCADE ,
+    film_id BIGINT NOT NULL REFERENCES film (id) ON DELETE CASCADE
+);
+
+INSERT INTO studio (name, date_of_foundation)
+VALUES ('Paramount Pictures', '1912-05-01'),
+       ('Walt Disney Studios', '1923-10-16'),
+       ('Warner Bros', '1923-01-01'),
+       ('20th Century Studios', '1935-05-31'),
+       ('Sony Pictures', '1991-08-07'),
+       ('Columbia Pictures', '1918-06-19'),
+       ('Universal Pictures', '1912-04-30');
