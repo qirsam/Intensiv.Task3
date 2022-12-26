0c72d80da61d08c1833a00a9605c9ceec12e7c02
qirsam <qirsam@gmail.com>
2022-12-26 16:56:12 +0300

    first

---
+++ src/main/java/com/qirsam/database/entity/Film.java
@@ -0,0 +1,11 @@
+package com.qirsam.database.entity;
+
+import java.time.LocalDate;
+
+public class Film {
+    private Long id;
+    private String name;
+    private LocalDate dateRelease;
+    private Studio studio;
+    private
+}
