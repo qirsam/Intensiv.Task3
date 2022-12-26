6ecb565fb3f4449c1c0775b59c3aa301400e68d6
Sergey Lazchenko <qirsam@gmail.com>
2022-12-26 19:42:18 +0300

    sql

--- src/main/resources/db.changelog/db.changelog-1.0.sql
+++ src/main/resources/db.changelog/db.changelog-1.0.sql
@@ -15,5 +15,17 @@ CREATE TABLE IF NOT EXISTS actor
     id         SERIAL      NOT NULL PRIMARY KEY,
     first_name VARCHAR(64) NOT NUll,
     last_name  VARCHAR(64) NOT NUll,
-    birth_date DATE
+    birth_date DATE,
+    sex        VARCHAR(16)
+);
+--rollback DROP TABLE actor
+
+--changeset qirsam:2
+CREATE TABLE IF NOT EXISTS film
+(
+    id           SERIAL                                          NOT NULL PRIMARY KEY,
+    name         VARCHAR(128)                                    NOT NULL,
+    date_release DATE,
+    studio_id    BIGINT REFERENCES studio (id) ON DELETE CASCADE NOT NULL
+
 )
